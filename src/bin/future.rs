6c2aed71ee819ac27391256d5dd8f4bb8095173b
liliang1 <li.liang@mydreamplus.com>
2018-08-22 17:24:16 +0800

    add some dependencies

---
+++ src/bin/future.rs
@@ -0,0 +1,53 @@
+extern crate futures;
+extern crate futures_cpupool;
+
+use futures::Future;
+use futures_cpupool::CpuPool;
+use futures_cpupool::CpuFuture;
+
+// prime numbers generator http://www.numberempire.com/primenumbers.php
+const BIG_PRIME: u64 = 1112298787;
+
+fn is_prime(num: u64) -> bool {
+    for i in 2..num {
+        if num % i == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn prime_future(num: u64) -> CpuFuture<bool, ()> {
+    let pool = CpuPool::new_num_cpus();
+
+    let result = pool.spawn_fn(move || {
+        let prime = is_prime(num);
+        let res: Result<bool, ()> = Ok(prime);
+        
+        res
+    });
+
+    result
+}
+
+fn main() {
+
+    let f = prime_future(BIG_PRIME).map(|x| if x {
+        println!("Future Prime")
+    } else {
+        println!("Future Not Prime")
+    });
+
+    // await here, will take double time. cause future wait will block the code.
+    // f.wait().unwrap();
+
+    if is_prime(BIG_PRIME) {
+        println!("Prime");
+    } else {
+        println!("Not Prime");
+    }
+
+    // await here, will take less time.
+    f.wait().unwrap();
+}
